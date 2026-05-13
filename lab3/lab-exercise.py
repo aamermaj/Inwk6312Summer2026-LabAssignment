5a444cc9ee03d94b68363a282101410a3853a9cc
student <aamermaj>
2026-05-13 16:16:42 -0300

    removed lab3 folder from lab1 branch

--- lab3/lab-exercise.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-import yaml
-
-ENV = Environment(loader=FileSystemLoader('.'))
-template = ENV.get_template("template-lab-exercise.j2")
-
-with open("data-lab-exercise.yml") as f:
-    data = yaml.load(f, Loader=yaml.SafeLoader)
-    print(template.render(routers=data['routers']))
